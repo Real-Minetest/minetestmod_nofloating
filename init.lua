7363d7106c82ffe8c5a867ad85d3c956e79fc830
zaoqi <zaomir@outlook.com>
2017-05-30 19:23:34 +0800

    U

--- init.lua
+++ init.lua
@@ -13,7 +13,69 @@
 
 --You should have received a copy of the GNU Affero General Public License
 --along with this program.  If not, see <http://www.gnu.org/licenses/>.
-require "haskell" ()
+local function eq(x, y)
+	if type(x) == "table" then
+		if type(y) ~= "table" then return false end
+		for k, v in pairs(x) do
+			if not eq (v, y[k]) then return false end
+		end
+		for k, v in pairs(y) do
+			if not eq (v, x[k]) then return false end
+		end
+		return true
+	else
+		return x == y
+	end
+end
+local function elem(x, xs)
+	if x == nil then return true end
+	for _, i in pairs(x) do
+		if eq(i, x) then return true end
+	end
+	return false
+end
+z={eq=eq,elem=elem}
+dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/api.lua")
 
+local n = 64
 nofloating = {}
+function nofloating.find(max, ...)
+	local r = {}
+	local n = {...}
+	while #n ~= 0 do
+		local a = {}
+		for _, x in ipairs(n) do
+			local name = minetest.get_node(x)
+			if not (name == "air" or name == "ignore" or elem(x, r)) then
+				if #r >= max then return r end
+				r[#r+1] = x
+				a[#a+1] = {x=x.x+1,y=x.y,z=x.z}
+				a[#a+1] = {x=x.x-1,y=x.y,z=x.z}
+				a[#a+1] = {x=x.x,y=x.y+1,z=x.z}
+				a[#a+1] = {x=x.x,y=x.y-1,z=x.z}
+				a[#a+1] = {x=x.x,y=x.y,z=x.z+1}
+				a[#a+1] = {x=x.x,y=x.y,z=x.z-1}
+			end
+		end
+		n = a
+	end
+	return r
+end
 
+function nofloating.check(...)
+	local x = nofloating.find(n, ...)
+	if #x < n then
+		for _, pos in ipairs(x) do
+			minetest.spawn_falling_node(pos)
+		end
+		return true
+	else
+		return false
+	end
+end
+
+local old_check_single_for_falling = minetest.check_single_for_falling
+function minetest.check_single_for_falling(pos)
+	nofloating.check(pos)
+	return minetest.check_single_for_falling(pos)
+end
